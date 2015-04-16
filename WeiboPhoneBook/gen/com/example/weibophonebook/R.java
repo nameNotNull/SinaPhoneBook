368676a5140e5da53930e911cea737aa164797c4
wangxuan <wangxuan6@staff.sina.com.cn>
2015-04-16 23:39:34 +0800

    sms ok

--- WeiboPhoneBook/gen/com/example/weibophonebook/R.java
+++ WeiboPhoneBook/gen/com/example/weibophonebook/R.java
@@ -21,14 +21,16 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int icon_tab_calllog_selected=0x7f020001;
-        public static final int icon_tab_contacts_selected=0x7f020002;
-        public static final int icon_tab_dialer_selected=0x7f020003;
-        public static final int icon_tab_msg_selected=0x7f020004;
+        public static final int avatar_default_48h=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int icon_tab_calllog_selected=0x7f020002;
+        public static final int icon_tab_contacts_selected=0x7f020003;
+        public static final int icon_tab_dialer_selected=0x7f020004;
+        public static final int icon_tab_msg_selected=0x7f020005;
+        public static final int logo=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
+        public static final int action_settings=0x7f08000a;
         public static final int bottom_left=0x7f080001;
         public static final int bottom_middle=0x7f080002;
         public static final int bottom_right=0x7f080003;
@@ -36,14 +38,17 @@ public final class R {
         public static final int color_title=0x7f080005;
         public static final int id_content=0x7f080000;
         public static final int lv=0x7f080006;
-        public static final int smsid=0x7f080007;
+        public static final int smsimg=0x7f080007;
+        public static final int smstext1=0x7f080008;
+        public static final int smstext2=0x7f080009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int callerfragment=0x7f030001;
         public static final int contact_list_items=0x7f030002;
         public static final int contactfragment=0x7f030003;
-        public static final int smsfragment=0x7f030004;
+        public static final int sms_list_items=0x7f030004;
+        public static final int smsfragment=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- WeiboPhoneBook/src/com/example/weibophonebook/SmsFragment.java
+++ WeiboPhoneBook/src/com/example/weibophonebook/SmsFragment.java
@@ -1,108 +1,132 @@
 package com.example.weibophonebook;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import android.app.Fragment;
+import android.app.ListFragment;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteException;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ScrollView;
-import android.widget.TextView;
-public class SmsFragment extends Fragment  
-{  
-  
-    @Override  
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,  
-            Bundle savedInstanceState)  
-    {  
-    	/*View v = inflater.inflate(R.layout.smsfragment, container, false);
-    	View tv = v.findViewById(R.id.smsid); 
-        ((TextView)tv).setText(getSmsInPhone());  
-        Log.i("a",getSmsInPhone());
-        return v;  */
-    	   ScrollView scroller = new ScrollView(getActivity());
-           TextView text = new TextView(getActivity());
-           scroller.addView(text);
-           text.setText(getSmsInPhone());
-           return scroller;
-    }
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
-    public int getShownIndex() {
-        return getArguments().getInt("index", 0);
-    }
-    public String getSmsInPhone() {  
-        final String SMS_URI_ALL = "content://sms/";  
-        final String SMS_URI_INBOX = "content://sms/inbox";  
-        final String SMS_URI_SEND = "content://sms/sent";  
-        final String SMS_URI_DRAFT = "content://sms/draft";  
-        final String SMS_URI_OUTBOX = "content://sms/outbox";  
-        final String SMS_URI_FAILED = "content://sms/failed";  
-        final String SMS_URI_QUEUED = "content://sms/queued";  
-  
-        StringBuilder smsBuilder = new StringBuilder();  
-  
-        try {  
-            Uri uri = Uri.parse(SMS_URI_ALL);  
-            String[] projection = new String[] { "_id", "address", "person", "body", "date", "type" };  
-            Cursor cur = getActivity().getContentResolver().query(uri, projection, null, null, "date desc");      // 获取手机内部短信  
-  
-            if (cur.moveToFirst()) {  
-                int index_Address = cur.getColumnIndex("address");  
-                int index_Person = cur.getColumnIndex("person");  
-                int index_Body = cur.getColumnIndex("body");  
-                int index_Date = cur.getColumnIndex("date");  
-                int index_Type = cur.getColumnIndex("type");  
-  
-                do {  
-                    String strAddress = cur.getString(index_Address);  
-                    int intPerson = cur.getInt(index_Person);  
-                    String strbody = cur.getString(index_Body);  
-                    long longDate = cur.getLong(index_Date);  
-                    int intType = cur.getInt(index_Type);  
-  
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");  
-                    Date d = new Date(longDate);  
-                    String strDate = dateFormat.format(d);  
-  
-                    String strType = "";  
-                    if (intType == 1) {  
-                        strType = "接收";  
-                    } else if (intType == 2) {  
-                        strType = "发送";  
-                    } else {  
-                        strType = "null";  
-                    }  
-  
-                    smsBuilder.append("[ ");  
-                    smsBuilder.append(strAddress + ", ");  
-                    smsBuilder.append(intPerson + ", ");  
-                    smsBuilder.append(strbody + ", ");  
-                    smsBuilder.append(strDate + ", ");  
-                    smsBuilder.append(strType);  
-                    smsBuilder.append(" ]\n\n");  
-                } while (cur.moveToNext());  
-  
-                if (!cur.isClosed()) {  
-                    cur.close();  
-                    cur = null;  
-                }  
-            } else {  
-                smsBuilder.append("no result!");  
-            } // end if  
-  
-            smsBuilder.append("getSmsInPhone has executed!");  
-  
-        } catch (SQLiteException ex) {  
-            Log.d("SQLiteException in getSmsInPhone", ex.getMessage());  
-        }  
-  
-        return smsBuilder.toString();  
-    } 
-}  
+public class SmsFragment extends ListFragment {
+	private ListView list;
+	private SimpleAdapter adapter;
+	private String TAG = SmsFragment.class.getName();
+
+	/**
+	 * @描述 在onCreateView中加载布局
+	 * */
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.smsfragment, container, false);
+		list = (ListView) view.findViewById(android.R.id.list);
+		Log.i(TAG, "--------onCreateView");
+		return view;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Log.i(TAG, "--------onCreate");
+		
+		adapter = new SimpleAdapter(getActivity(), getSmsInPhone(),
+				R.layout.sms_list_items, new String[] { "title","content" },
+				new int[] { R.id.smstext1 ,R.id.smstext2});
+		setListAdapter(adapter);
+
+	}
+
+
+	public List<? extends Map<String, ?>>  getSmsInPhone() {
+		final String SMS_URI_ALL = "content://sms/";
+		final String SMS_URI_INBOX = "content://sms/inbox";
+		final String SMS_URI_SEND = "content://sms/sent";
+		final String SMS_URI_DRAFT = "content://sms/draft";
+		final String SMS_URI_OUTBOX = "content://sms/outbox";
+		final String SMS_URI_FAILED = "content://sms/failed";
+		final String SMS_URI_QUEUED = "content://sms/queued";
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		
+		
+			
+		StringBuilder smsBuilder = new StringBuilder();
+
+		try {
+			Uri uri = Uri.parse(SMS_URI_ALL);
+			String[] projection = new String[] { "_id", "address", "person",
+					"body", "date", "type" };
+			Cursor cur = getActivity().getContentResolver().query(uri,
+					projection, null, null, "date desc"); // 获取手机内部短信
+
+			if (cur.moveToFirst()) {
+				int index_Address = cur.getColumnIndex("address");
+				int index_Person = cur.getColumnIndex("person");
+				int index_Body = cur.getColumnIndex("body");
+				int index_Date = cur.getColumnIndex("date");
+				int index_Type = cur.getColumnIndex("type");
+
+				do {
+					String strAddress = cur.getString(index_Address);
+					int intPerson = cur.getInt(index_Person);
+					String strbody = cur.getString(index_Body);
+					long longDate = cur.getLong(index_Date);
+					int intType = cur.getInt(index_Type);
+
+					SimpleDateFormat dateFormat = new SimpleDateFormat(
+							"yyyy-MM-dd hh:mm:ss");
+					Date d = new Date(longDate);
+					String strDate = dateFormat.format(d);
+
+					String strType = "";
+					if (intType == 1) {
+						strType = "接收";
+					} else if (intType == 2) {
+						strType = "发送";
+					} else {
+						strType = "null";
+					}
+
+					smsBuilder.append("[ ");
+					smsBuilder.append(strAddress + ", ");
+					smsBuilder.append(intPerson + ", ");
+					smsBuilder.append(strbody + ", ");
+					smsBuilder.append(strDate + ", ");
+					smsBuilder.append(strType);
+					smsBuilder.append(" ]\n\n");
+					String strsString = strAddress+intPerson+strbody+strDate+strType;
+					Map<String, Object> map = new HashMap<String, Object>();
+				
+					map.put("title", strAddress);
+					map.put("content", strbody);
+					list.add(map);
+				} while (cur.moveToNext());
+
+				if (!cur.isClosed()) {
+					cur.close();
+					cur = null;
+				}
+			} else {
+				smsBuilder.append("no result!");
+			} // end if
+
+			smsBuilder.append("getSmsInPhone has executed!");
+
+		} catch (SQLiteException ex) {
+			Log.d("SQLiteException in getSmsInPhone", ex.getMessage());
+		}
+
+		return list;
+	}
+}
